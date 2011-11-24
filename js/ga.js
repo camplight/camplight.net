ce003fa5288380213d6d6e98fc083326c66d4593
Vassil Kalkov <vassilkalkov@gmail.com>
2011-11-24 18:30:05 +0200

    Adeed ga.js-copied from google , renamed ga.coffee , not using it

--- js/ga.js
+++ js/ga.js
@@ -1,22 +1,12 @@
-(function() {
-  var _gaq;
 
-  _gaq = _gaq || [];
-
+  var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-27240300-1']);
-
   _gaq.push(['_trackPageview']);
 
   (function() {
-    var ga, s, _ref;
-    ga = document.createElement('script');
-    ga.type = 'text/javascript';
-    ga.async = true;
-    ga.src = ((_ref = 'https:' === document.location.protocol) != null ? _ref : {
-      'https://ssl': 'http://www'
-    }) + '.google-analytics.com/ga.js';
-    s = document.getElementsByTagName('script')[0];
-    return s.parentNode.insertBefore(ga, s);
-  });
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 
-}).call(this);
+
