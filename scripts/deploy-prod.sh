b773bce2f69e2fa6af0adb642c41f8a916c693b9
Boris Filipov <obiwonn@gmail.com>
2011-11-30 11:50:05 +0200

    upgrading deply-prod script

--- scripts/deploy-prod.sh
+++ scripts/deploy-prod.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # example: 
-# /projects/camplight.net$ ./scripts/deploy-prod.sh <user>@<server>:<path>
-target=$1
+# /projects/camplight.net$ ./scripts/deploy-prod.sh <user> <server> <path>
+user=$1
+server=$2
+target=$3
 me=`dirname $0`
-rsync -axvzco --exclude-from=$me/deploy-prod.exclude $me/../ $target
+ssh $user@$server "sudo chown $user:$user -R $target"
+rsync -axvzco --exclude-from=$me/deploy-prod.exclude $me/../ $user@$server:$target
