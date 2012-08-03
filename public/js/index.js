554b0083d5a1e6f768533fdf594beda962edf980
Boris Filipov <obiwonn@gmail.com>
2012-08-03 19:12:26 +0300

    smoother landing elements animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
+  $("#header").css({
+    height: "0px"
+  }).show().animate({
+    height: "224px"
+  });
+
   $("#bottom").css({
     opacity: "0"
   }).show().delay(1400).animate({
@@ -12,21 +18,15 @@ $(document).ready(function(){
     opacity: "1"
   }, 200);
 
-  $("#header").css({
-    height: "0px"
-  }).show().animate({
-    height: "224px"
-  });
-
   $("#skills").css({
     height: "0px"
   }).delay(1800).show().animate({
     height: "300px"
-  });
+  }, 600);
 
   $("#team").css({
     height: "0px"
-  }).delay(3600).show().animate({
+  }).delay(2600).show().animate({
     height: "300px"
-  });
+  }, 600);
 });
