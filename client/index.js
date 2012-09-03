48f9d8b01c6af06ccdef61c8520c687b6bc56a89
Boris Filipov <obiwonn@gmail.com>
2012-09-03 21:53:33 +0300

    adding some effects

--- client/index.js
+++ client/index.js
@@ -2,4 +2,9 @@ require("ga");
 $ = require("jquery-browserify");
 
 $(document).ready(function(){
+  var target = $('[effect="dropin"]');
+  target.hide();
+  for(var i = 0; i<target.length; i++) {
+    $(target[i]).delay(i*50).fadeIn();
+  }
 });
