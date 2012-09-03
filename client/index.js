fad3ed8130154d6fc65c426a5261e804b6681207
Boris Filipov <obiwonn@gmail.com>
2012-09-03 16:31:16 +0300

    additinal improvements

--- client/index.js
+++ client/index.js
@@ -2,33 +2,8 @@ require("ga");
 $ = require("jquery-browserify");
 
 $(document).ready(function(){
-  /*$("#header").css({
-    height: "0px"
-  }).show().animate({
-    height: "224px"
-  });
-
-  $("#bottom").css({
-    opacity: "0"
-  }).show().delay(1400).animate({
-    opacity: "1"
-  }, 200);
-
-  $("#footer").css({
-    opacity: "0"
-  }).show().delay(1600).animate({
-    opacity: "1"
-  }, 200);
-
-  $("#skills").css({
-    height: "0px"
-  }).delay(1800).show().animate({
-    height: "300px"
-  }, 600);
-
-  $("#team").css({
-    height: "0px"
-  }).delay(2600).show().animate({
-    height: "300px"
-  }, 600);*/
+  if($(document).height() < 800)
+    $("#bottom").css({position: "fixed"});
+  else
+    $("#bottom").css({position: "relative"});
 });
