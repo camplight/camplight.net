3445ae8472f186b7fcfb7b6cf8430a84daf65e5f
Boris Filipov <obiwonn@gmail.com>
2012-09-03 22:54:02 +0300

    adding more effects

--- client/index.js
+++ client/index.js
@@ -1,10 +1,34 @@
 require("ga");
-$ = require("jquery-browserify");
+jQuery = $ = require("jquery-browserify");
+require("./libs/jquery.animate-colors-min");
 
-$(document).ready(function(){
-  var target = $('[effect="dropin"]');
+var showAll = function(){
+  var target = $('[effect-show="dropin"]');
   target.hide();
   for(var i = 0; i<target.length; i++) {
     $(target[i]).delay(i*50).fadeIn();
   }
+}
+
+var hoverAll = function(){
+  var target = $('[effect-hover="blink"]');
+  target.mouseenter(function(){
+    $(".fn", this)
+      .animate({
+        color: "#AAA"
+      })
+      .delay(10)
+      .animate({
+        color: "#333"
+      });
+  });
+}
+
+$(document).ready(function(){
+  $("#header .wrap").html($("#animatedLogoTemplate").html());
+  $(".logoTop").animate({ opacity: "1", top: "20px" }, 600);
+  $(".logoShadow").animate({ opacity: "1" }, 600);
+  $(".logoCamplightText").delay(800).animate({ opacity: "1" }, 600);
+  showAll();
+  hoverAll();
 });
