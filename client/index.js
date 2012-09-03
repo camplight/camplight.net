e880bdc016518dfd85041b8cd29805ee72bf970d
Boris Filipov <obiwonn@gmail.com>
2012-09-03 18:39:44 +0300

    fixes and adding content

--- client/index.js
+++ client/index.js
@@ -2,8 +2,5 @@ require("ga");
 $ = require("jquery-browserify");
 
 $(document).ready(function(){
-  if($(document).height() < 800)
-    $("#bottom").css({position: "fixed"});
-  else
-    $("#bottom").css({position: "relative"});
+  
 });
