5b3632ee90fc5f7f4da4e2550ce0d56e25aaa07e
Boris Filipov <obiwonn@gmail.com>
2012-09-04 15:43:41 +0300

    adding idle flame animation

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 require("ga");
 jQuery = $ = require("jquery-browserify");
 require("./libs/jquery.animate-colors-min");
+require("./libs/jquery.idle-timer");
 
 var showAll = function(){
   var target = $('[effect-show="dropin"]');
@@ -23,6 +24,26 @@ var hoverAll = function(){
   });
 }
 
+var animateWhenIdle = function(){
+  var target = $('[effect-hover="blink"]');
+  if(target.length == 0)
+    return;
+
+  var animationIntervalID;
+  $.idleTimer(5000);
+  $(document).bind("idle.idleTimer", function(){
+    animationIntervalID = setInterval(function(){
+      for(var i = target.length-1; i>=0; i--) {
+        $(".icon.colored", target[i]).delay((target.length-i)*50).fadeIn().delay((target.length-i)*10).fadeOut();
+      }
+    }, 4000);
+  });
+  $(document).bind("active.idleTimer", function(){
+    if(animationIntervalID)
+      clearInterval(animationIntervalID);
+  });
+}
+
 $(document).ready(function(){
   $("#header .wrap").html($("#animatedLogoTemplate").html());
   $(".logoTop").animate({ opacity: "1", top: "20px" }, 600);
@@ -30,4 +51,5 @@ $(document).ready(function(){
   $(".logoCamplightText").delay(800).animate({ opacity: "1" }, 600);
   showAll();
   hoverAll();
+  animateWhenIdle();
 });
