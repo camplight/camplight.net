3f19fec1cb4733db2dd2fbb00ac437b4bb33fbc1
Boris Filipov <obiwonn@gmail.com>
2012-10-21 01:42:21 +0300

    adding mobile skrollr

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ require("./vendor/ga");
 jQuery = $ = require("jquery-browserify");
 require("./vendor/jquery.animate-colors-min");
 require("./vendor/jquery.idle-timer");
+require("./vendor/skrollr.mobile");
 
 $(document).ready(function(){
   $(".invisible").css({"opacity": 0});
