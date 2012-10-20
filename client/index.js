78df6beace3eee62281d2f2b20243e0690ee886b
Boris Filipov <obiwonn@gmail.com>
2012-10-21 02:08:06 +0300

    mobile fixes

--- client/index.js
+++ client/index.js
@@ -2,15 +2,16 @@ require("./vendor/ga");
 jQuery = $ = require("jquery-browserify");
 require("./vendor/jquery.animate-colors-min");
 require("./vendor/jquery.idle-timer");
-require("./vendor/skrollr.mobile");
+
 
 $(document).ready(function(){
   $(".invisible").css({"opacity": 0});
   require("./vendor/skrollr.min.js");
+  require("./vendor/skrollr.mobile");
   skrollr.init({
     forceHeight: true,
     render: function(data){
-      console.log(data);
+      //console.log(data);
     }
   });
 });
