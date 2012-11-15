e49d5fa8f620b8b4414eeb0c3b30ce1a37221be5
Boris Filipov <obiwonn@gmail.com>
2012-11-15 18:43:20 +0200

    added some improvements in animation keyframing

--- client/index.js
+++ client/index.js
@@ -10,78 +10,105 @@ $(document).ready(function(){
 
   require("./vendor/skrollr.min");
   require("./vendor/skrollr.mobile");
-  require("./vendor/impress");
+  /*require("./vendor/impress");
 
   var prevActiveMenu;
   var api = impress();
-  api.init();
+  api.init();*/
+
+  var screens = {
+    landing: 1,
+    about: 0.8,
+    about2: 0.7,
+    about3: {ratio: 0.6, nextDelayWith: 500},
+    skills: {ratio: 1, nextDelayWith: 500},
+    members: 1,
+    partners: 1,
+    bonus: 1
+  };
+
+var screensOrder = [
+    "landing",
+    "about",
+    "about2",
+    "about3",
+    "skills",
+    "members",
+    "partners",
+    "bonus"
+  ];  
+
+  var offset = 0;
+  for(var i = 0;i < screensOrder.length;i ++) {
+    var ratio = screens[screensOrder[i]];
+    screens[screensOrder[i]] = offset;
+    if(typeof ratio == "object")
+      offset += $(document).height()*ratio.ratio+ratio.nextDelayWith;  
+    else
+      offset += $(document).height()*ratio;
+  }
+  console.log(screens);
 
   s = skrollr.init({
     beforerender: function(data){
       console.log(data.curTop);
-      if(prevActiveMenu)
-        prevActiveMenu.removeClass("active");
-      if(data.curTop<300)
-        prevActiveMenu = $("#menu a[href='#']").addClass("active");
-      else
-      if(data.curTop<786) 
-        prevActiveMenu = $("#menu a[href='#about']").addClass("active");
-      else
-      if(data.curTop<2100) 
-        prevActiveMenu = $("#menu a[href='#resources']").addClass("active");
-      else
-      if(data.curTop<3200) 
-        prevActiveMenu = $("#menu a[href='#members']").addClass("active");
-      else
-      if(data.curTop<3900) 
-        prevActiveMenu = $("#menu a[href='#partners']").addClass("active");
-      else
-      if(data.curTop>7000 && data.curTop<7300) 
-        prevActiveMenu = $("#menu a[href='#contacts']").addClass("active");
     },
-    constants: {
-      landing: 0,
-      skills: 500,
-      members: 1300,
-      bonus: 3500
-    }
+    constants: screens
   });
-
   
-  var setScrollTop = function(top) {
+  var setScrollTop = function(top, options) {
     if(isMobile) {
       skrollr.iscroll.scrollTo(0,-top);
     } else
-      s.setScrollTop(top);
+      s.animateTo(top, options);
+  }
+
+  var currentScreenIndex = 0;
+  var playNextScreen = function(){
+    currentScreenIndex += 1;
+    if(currentScreenIndex>=screensOrder.length)
+      currentScreenIndex = 0;
+    var top = screens[screensOrder[currentScreenIndex]];
+    setScrollTop(top);
+  }
+
+  var playToScreen = function(index, options){
+    currentScreenIndex = index;
+    var top = screens[screensOrder[currentScreenIndex]];
+    setScrollTop(top, options); 
   }
 
   var AppRouter = Backbone.Router.extend({
     routes: {
       "": "start",
       "about": "showAbout",
-      "resources": "showResourcesOrSkills",
-      "skills": "showResourcesOrSkills",
+      "about2": "showAbout2",
+      "about3": "showAbout3",
+      "skills": "showSkills",
       "members": "showMembers",
       "partners": "showPartners",
       "contacts": "showContacts"
     },
     start: function(){
-      setScrollTop(0)
+      playToScreen(0);
     },
     showAbout: function(){
-      setScrollTop(786);
+      playToScreen(1);
     },
-    showResourcesOrSkills: function(){
-      setScrollTop(1600);
+    showAbout2: function(){
+      playToScreen(2);
+    },
+    showAbout3: function(){
+      playToScreen(3);
+    },
+    showSkills: function(){
+      playToScreen(4);
     },
     showMembers: function(){
-      setScrollTop(2500);
+      playToScreen(5);
     },
     showPartners: function(){
-      setScrollTop(3600);
-    },
-    showContacts: function(){
-      setScrollTop(7300);
+      playToScreen(6);
     }
   });
 
@@ -94,10 +121,21 @@ $(document).ready(function(){
   }
   Backbone.history.start({navigate: true}); 
 
-  if(isMobile) {
-    $(".content").on("click", function(e){
-      scrollNext();
+  $("#menu a").click(function(e){
+    e.preventDefault();
+    var self = this;
+    playToScreen($(this).attr("data-screenIndex"), {
+      done: function(){
+        app.router.navigate($(self).attr("href"), false);
+      }
     });
-  }
-    
+    return false;
+  })
+
+  $(window).mousedown(function(e){
+    if(e.ctrlKey) {
+      e.preventDefault();
+      playNextScreen();
+    }
+  });
 });
