0785e313626e93a0162e492bbded2334bc527d33
Boris Filipov <obiwonn@gmail.com>
2013-01-10 11:33:44 +0200

    0.0.39 release

---
+++ scripts/release-live.js
@@ -0,0 +1,38 @@
+require('shelljs/global');
+
+var p = require("../package.json");
+var newVersion = p.version.split(".");
+newVersion[2] = (parseInt(newVersion[2])+1).toString();
+newVersion = newVersion.join(".");
+cd(__dirname+"/../");
+
+if(exec("git pull --ff origin master").code != 0){
+  echo("Error: failed to pull origin master");
+  exit(1);
+}
+
+// TODO find out how to trap errors from sed bellow
+sed('-i', '"version": "'+p.version+'"', '"version": "'+newVersion+'"', "package.json");
+
+if(exec("git add --all").code != 0){
+  echo("Error: failed to git add --all");
+  exit(1);
+}
+
+if(exec("git commit -am '"+newVersion+" release'").code != 0){
+  echo("Error: failed to commit version bump");
+  exit(1);
+}
+
+if(exec("git push").code != 0){
+  echo("Error: failed to git push");
+  exit(1);
+}
+
+
+if(exec("git push upstream master").code != 0){
+  echo("Error: failed to git push");
+  exit(1);
+}
+
+require("./restart-live");
